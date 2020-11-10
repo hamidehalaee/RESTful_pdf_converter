1ecf67437b8fb4e4610bdf1d4d5a11d390218119
hamidehalaee <hamidehalaee.name>
2020-11-10 10:41:04 -0800

    add project,using pdfkit library

---
+++ index.js
@@ -0,0 +1,27 @@
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+// Create a document
+const doc = new PDFDocument;
+
+// Pipe its output somewhere, like to a file or HTTP response
+// See below for browser usage
+doc.pipe(fs.createWriteStream('output.pdf'));
+
+// Embed a font, set the font size, and render some text
+doc.fontSize(25)
+   .text('Some text with an embedded font!', 100, 100);
+
+
+// Add another page
+doc.addPage()
+   .fontSize(25)
+   .text('add another page', 100, 100);
+
+// Add some text with annotations
+doc.addPage()
+   .fillColor("blue")
+   .text('Here is a link!', 100, 100)
+  
+
+// Finalize PDF file
+doc.end();
