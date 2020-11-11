42745cb08e17b624b826753ee267ba889b48ef7d
hamidehalaee <hamidehalaee.name>
2020-11-11 09:37:33 -0800

    now the name of output will change to random numbers plus m foravoiding same name output

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const doc = new PDFDocument;
 
 // Pipe its output somewhere, like to a file or HTTP response
 // See below for browser usage
-doc.pipe(fs.createWriteStream('output.pdf'));
+doc.pipe(fs.createWriteStream(`${Math.floor(Math.random() * 100) + 1 + "m" + ".pdf"}`));
 
 // Embed a font, set the font size, and render some text
 doc.fontSize(25)
